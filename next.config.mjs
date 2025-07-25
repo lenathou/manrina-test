541a9824edd5b1a5e5259d65537113da6a5b097e
Nath <lenathou972@gmail.com>
2025-07-25 14:49:42 +0200

    u

--- next.config.mjs
+++ next.config.mjs
@@ -38,10 +38,8 @@ const nextConfig = {
     webpack: (config) => {
         config.resolve.alias = {
             ...(config.resolve.alias || {}),
-            // Transform all direct `react-native` imports to `react-native-web`
             'react-native$': 'react-native-web',
-            // Add the @ alias
-            '@': path.resolve(__dirname, 'src'),
+            '@': path.resolve(__dirname, 'src'), // Ajoutez cette ligne
         };
         return config;
     },
